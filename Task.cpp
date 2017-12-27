fcf3354380405a46925af57b45e5a8b969205bc3
Ivan Novakov <ivan.novakov.dev@gmail.com>
2017-12-27 18:17:40 +0200

    Add simple scheduler

--- Task.cpp
+++
@@ -1,8 +0,0 @@
-#include "Task.hpp"
-
-Task::Task(const std::string &command) : command(command) {}
-
-void Task::operator()()
-{
-    system(command.c_str());
-}
---
+++ cli.hpp
@@ -0,0 +1,8 @@
+//
+// Created by ivan-novakov on 12/27/17.
+//
+
+#ifndef SECMAN_CLI_HPP
+#define SECMAN_CLI_HPP
+
+#endif //SECMAN_CLI_HPP
